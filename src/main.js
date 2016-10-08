2e8596b37184433bbbbad3adc397bec220064e2d
Brian Chirgwin <bchirgwin@yahoo.com>
2016-10-07 23:10:25 -0500

    hapi helloworld

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
-console.log('Hello, World!');
+'use strict';
+var Hapi = require('hapi');
+
+var server = new Hapi.Server();
+
+server.connection({port: 3000});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello World! from Hapi');
+    }
+});
+
+server.start(function (err) {
+    if (err) {
+        throw err;
+    }
+    console.log('Server running at ', server.info.port);
+});
